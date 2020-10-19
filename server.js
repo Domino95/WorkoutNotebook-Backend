a405f51eb86295f735869033fe40c731e0d3d4ca
Dominik Swiatek <dominik.swiatek1995@gmail.com>
2020-10-19 17:57:21 +0200

    fix port declaration

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const bodyParser = require('body-parser')
-const PORT = process.env.port || 4000
+const PORT = process.env.PORT || 4000
 const mongoose = require('mongoose')
 const { graphqlHTTP } = require('express-graphql');
 const graphqlSchema = require('./graphql/schema/index')
